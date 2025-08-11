493e0b27a22cda9d17b502227f38c8c95a596880
Daniels Heincis <63064473+DanielsHeincis@users.noreply.github.com>
2025-08-11 12:30:12 +0000

    Add files via upload
    Added stuff

---
+++ identity_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+for i in {1..3}; do
+    sudo -n whoami
+    sleep 25
+done
+
---
+++ timeskip_30s.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "[*] Fast-forwarding system time by 30 seconds..."
+sudo date -s "+30 seconds"
+
+echo "[*] Done. System time is now:"
+date
+
